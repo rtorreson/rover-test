6cf05920588a029a847dc53102de4d4aa9b1438a
Rafael Torreson <rafael.torreson@hotmail.com>
2024-03-13 20:13:26 -0300

    adding jsdoc in singleton instance

--- src/singleton/plateau.singleton.js
+++ src/singleton/plateau.singleton.js
@@ -2,31 +2,50 @@
  * Represents a plateau on Mars.
  */
 export class Plateau {
-    /**
-     * Create a Plateau instance.
-     * @param {number} x - The width of the plateau.
-     * @param {number} y - The height of the plateau.
-     */
-    constructor(x, y) {
-        /**
-         * The width of the plateau.
-         * @type {number}
-         */
-        this.x = x;
-        /**
-         * The height of the plateau.
-         * @type {number}
-         */
-        this.y = y;
+  /**
+   * Create a Plateau instance.
+   * @param {number} x - The width of the plateau.
+   * @param {number} y - The height of the plateau.
+   */
+  constructor(x, y) {
+    if (Plateau.instance) {
+      return Plateau.instance;
     }
 
     /**
-     * Check if a position is within the bounds of the plateau.
-     * @param {number} x - The x-coordinate to check.
-     * @param {number} y - The y-coordinate to check.
-     * @returns {boolean} True if the position is within bounds, false otherwise.
+     * The width of the plateau.
+     * @type {number}
      */
-    withinBounds(x, y) {
-        return x >= 0 && x <= this.x && y >= 0 && y <= this.y;
+    this.x = x;
+    /**
+     * The height of the plateau.
+     * @type {number}
+     */
+    this.y = y;
+
+    Plateau.instance = this;
+  }
+
+  /**
+   * Check if a position is within the bounds of the plateau.
+   * @param {number} x - The x-coordinate to check.
+   * @param {number} y - The y-coordinate to check.
+   * @returns {boolean} True if the position is within bounds, false otherwise.
+   */
+  withinBounds(x, y) {
+    return x >= 0 && x <= this.x && y >= 0 && y <= this.y;
+  }
+
+  /**
+   * Get the singleton instance of the Plateau.
+   * @param {number} x - The width of the plateau.
+   * @param {number} y - The height of the plateau.
+   * @returns {Plateau} The singleton instance of the Plateau.
+   */
+  static instance(x, y) {
+    if (!Plateau.instance) {
+      Plateau.instance = new Plateau(x, y);
     }
+    return Plateau.instance;
+  }
 }
