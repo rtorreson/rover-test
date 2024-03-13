36e8214b24a92da09a183a5739adddb4cccf1dba
Rafael Torreson <rafael.torreson@hotmail.com>
2024-03-13 18:23:08 -0300

    update imports

--- test/integration.test.js
+++ test/integration.test.js
@@ -1,4 +1,4 @@
-import { strictEqual } from 'assert';
+import { strictEqual } from 'node:assert';
 import { RoverNavigator } from '../src/strategy/navigate.strategy.js';
 
 describe('Integration Test for navigateRovers function', () => {
